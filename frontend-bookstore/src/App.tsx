b4ebafc0e0df0ee9022832925f07f69cde1c3352
tanjir-mahabub <md.tanjir.mahabub@gmail.com>
2023-12-12 11:56:28 +0600

    added page loader in frontend

--- frontend-bookstore/src/App.tsx
+++ frontend-bookstore/src/App.tsx
@@ -1,9 +1,21 @@
-import { ToastContainer } from "react-toastify"
-import BookList from "./components/BookList.tsx"
-import Footer from "./components/Footer.tsx"
-import Header from "./components/Header"
+import { ToastContainer } from "react-toastify";
+import React, { useState, useEffect } from "react";
+import BookList from "./components/BookList.tsx";
+import Footer from "./components/Footer.tsx";
+import Header from "./components/Header";
+import PageLoader from "./components/PageLoader";
 
 const App = () => {
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      await new Promise((resolve) => setTimeout(resolve, 2000));
+      setIsLoading(false);
+    };
+
+    fetchData();
+  }, []);
 
   return (
     <>
@@ -11,12 +23,12 @@ const App = () => {
         <Header />
         <main className="container mx-auto relative top-[50px]">
           <ToastContainer autoClose={3000} />
-          <BookList />
+          {isLoading ? <PageLoader /> : <BookList />}
           <Footer />
         </main>
       </section>
     </>
-  )
-}
+  );
+};
 
-export default App
+export default App;
---
+++ frontend-bookstore/src/components/PageLoader.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const PageLoader: React.FC = () => {
+    return (
+        <div className="loader-container">
+            <div className="loader"></div>
+        </div>
+    );
+};
+
+export default PageLoader;
